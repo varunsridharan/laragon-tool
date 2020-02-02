e39544b7bcdedea8d5d8b7069a28e347ca7a50e9
Varun <varunsridharan23@gmail.com>
2020-02-02 14:55:28 +0530

    Updated Menus.

--- parts/menu.php
+++ parts/menu.php
@@ -5,17 +5,22 @@
 	</button>
 
 	<div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+		<a class="navbar-brand" href="#">Laragon ToolKit</a>
 		<ul class="navbar-nav">
-			<li class="nav-item"><a class="nav-link" href="settings.php">Settings</a></li>
 
+			<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+
+			<li class="nav-item"><a class="nav-link" href="hosts.php">Hosts</a></li>
 
 			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true"
-				   aria-expanded="false">Dropdown</a>
+				<a class="nav-link dropdown-toggle" href="#" id="settings" data-toggle="dropdown" aria-haspopup="true"
+				   aria-expanded="false">Settings</a>
 				<div class="dropdown-menu" aria-labelledby="dropdown08">
-					<a class="dropdown-item" href="#">Action</a>
-					<a class="dropdown-item" href="#">Another action</a>
-					<a class="dropdown-item" href="#">Something else here</a>
+					<a class="dropdown-item" href="settings.php#general_config">General</a>
+					<a class="dropdown-item" href="settings.php#host_file_config">Hosts File</a>
+					<a class="dropdown-item" href="settings.php#library_config_data">Library</a>
+					<a class="dropdown-item" href="settings.php#apache_config_data">Apache</a>
+					<a class="dropdown-item" href="settings.php#nginx_config_data">Nginx</a>
 				</div>
 			</li>
 		</ul>
