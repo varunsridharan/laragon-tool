168db8a457751434f6800181bb0bdb9a22486333
Varun <varunsridharan23@gmail.com>
2020-02-02 13:58:04 +0530

    Added Option to update settings.

--- settings.php
+++ settings.php
@@ -1,7 +1,12 @@
 <?php
-
 require __DIR__ . '/config.php';
+
+if ( isset( $_POST['update_settings'] ) ) {
+	$instance = new \VSP\Laragon\Modules\Settings\Save();
+	echo $instance->alerts();
+}
 ?>
+
 	<form id="settingsform" method="post" class="needs-validation" novalidate>
 
 		<div class="accordion" id="settings_accordion">
@@ -24,7 +29,8 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend"><span
 											class="input-group-text">Larangon Install Path</span></div>
 								<input type="text" class="form-control" name="laragon_path"
-									   placeholder="Full Path on where laragon is installed" required>
+									   placeholder="Full Path on where laragon is installed"
+									   value="<?php echo get_option( 'laragon_path' ); ?>" required>
 							</div>
 						</div>
 
@@ -33,7 +39,8 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend"><span class="input-group-text">Document Root</span>
 								</div>
 								<input type="text" class="form-control" name="document_root"
-									   placeholder="${LARAGON_PATH}/www" required>
+									   placeholder="${LARAGON_PATH}/www" required
+									   value="<?php echo get_option( 'document_root' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted mb-3"> use <code>${LARAGON_PATH}</code> to
 								locate config
@@ -45,16 +52,16 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend"><span
 											class="input-group-text">Vhosts File Prefix</span></div>
 								<input type="text" class="form-control" name="host_file_prefix" placeholder="auto-"
-									   required>
+									   required value="<?php echo get_option( 'host_file_prefix' ); ?>">
 							</div>
 						</div>
 
 						<div class="form-row">
 							<div class="input-group col-md-12 mb-2">
 								<div class="input-group-prepend"><span class="input-group-text">PLATFORM</span></div>
-								<input type="text" class="form-control" name="host_file_prefix"
+								<input type="text" class="form-control" name="platform"
 									   placeholder="windows,linux,mac"
-									   required>
+									   required value="<?php echo get_option( 'platform', 'windows' ); ?>">
 							</div>
 						</div>
 					</div>
@@ -65,13 +72,12 @@ require __DIR__ . '/config.php';
 				<div class="card-header" id="host_file_config">
 					<h2 class="mb-0">
 						<button class="btn btn-link" type="button" data-toggle="collapse"
-								data-target="#host_file_config_data">
-							Hosts File Config
+								data-target="#host_file_config_data">Hosts File Config
 						</button>
 					</h2>
 				</div>
 
-				<div id="host_file_config_data" class="collapse" aria-labelledby="host_file_config"
+				<div id="host_file_config_data" class="collapse show" aria-labelledby="host_file_config"
 					 data-parent="#settings_accordion">
 					<div class="card-body">
 						<div class="form-row">
@@ -79,7 +85,7 @@ require __DIR__ . '/config.php';
 								<div class="custom-control custom-switch">
 									<input type="checkbox" class="custom-control-input" name="hosts_file_entry"
 										   id="hosts_file_entry"
-										   required>
+										   required <?php echo ( ! empty( get_option( 'hosts_file_entry', 'on' ) ) ) ? 'checked' : ''; ?>>
 									<label class="custom-control-label" for="hosts_file_entry">Auto Add Hosts File
 										Entry</label>
 								</div>
@@ -91,7 +97,8 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend"><span
 											class="input-group-text">Host File Location</span></div>
 								<input type="text" class="form-control" name="hosts_file"
-									   placeholder="C:\Windows\System32\drivers\etc/hosts" required>
+									   placeholder="C:\Windows\System32\drivers\etc/hosts" required
+									   value="<?php echo get_option( 'hosts_file' ); ?>">
 							</div>
 						</div>
 					</div>
@@ -108,7 +115,7 @@ require __DIR__ . '/config.php';
 					</h2>
 				</div>
 
-				<div id="library_config_data" class="collapse" aria-labelledby="library_config"
+				<div id="library_config_data" class="collapse show" aria-labelledby="library_config"
 					 data-parent="#settings_accordion">
 					<div class="card-body">
 						<div class="form-row">
@@ -116,13 +123,16 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend">
 									<span class="input-group-text">MK Cert Location</span>
 								</div>
-								<input type="text" class="form-control" name="mkcert_path" placeholder="default"
-									   required>
+								<input type="text" class="form-control" name="library[mkcert_path]"
+									   placeholder="${LARAGON_TOOLKIT_PATH}/library/mkcert.exe"
+									   required
+									   value="<?php echo get_option( 'library/mkcert_path', '${LARAGON_TOOLKIT_PATH}/library/mkcert.exe' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted">
 								if you have <a href="https://github.com/FiloSottile/mkcert">mkcert</a> already installed
 								please
-								provide the exact location or enter <strong>default</strong> to use prepacked source
+								provide the exact location or enter
+								<code>${LARAGON_TOOLKIT_PATH}/library/mkcert.exe</code> to use prepacked source
 							</small>
 						</div>
 					</div>
@@ -139,7 +149,7 @@ require __DIR__ . '/config.php';
 					</h2>
 				</div>
 
-				<div id="apache_config_data" class="collapse" aria-labelledby="apache_config"
+				<div id="apache_config_data" class="collapse show" aria-labelledby="apache_config"
 					 data-parent="#settings_accordion">
 					<div class="card-body">
 						<div class="form-row">
@@ -149,7 +159,7 @@ require __DIR__ . '/config.php';
 								</div>
 								<input type="text" class="form-control" name="apache[config]"
 									   placeholder="${LARAGON_PATH}/etc/apache2/"
-									   required>
+									   required value="<?php echo get_option( 'apache/config' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted mb-3"> use <code>${LARAGON_PATH}</code> to
 								locate config
@@ -162,10 +172,11 @@ require __DIR__ . '/config.php';
 									<span class="input-group-text">Sites Config</span>
 								</div>
 								<input type="text" class="form-control" name="apache[sites_enabled]"
-									   placeholder="${APACHE_LOCATION}/sites-enabled/" required>
+									   placeholder="${APACHE_PATH}/sites-enabled/" required
+									   value="<?php echo get_option( 'apache/sites_enabled' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted mb-3">
-								use <code>${APACHE_LOCATION}</code> to Apache config inside current install path. <br/>
+								use <code>${APACHE_PATH}</code> to Apache config inside current install path. <br/>
 								use <code>${LARAGON_PATH}</code> to locate config inside current install path.
 							</small>
 						</div>
@@ -176,11 +187,11 @@ require __DIR__ . '/config.php';
 									<span class="input-group-text">Alias Config</span>
 								</div>
 								<input type="text" class="form-control" name="apache[alias]"
-									   placeholder="${APACHE_LOCATION}/alias/"
-									   required>
+									   placeholder="${APACHE_PATH}/alias/"
+									   required value="<?php echo get_option( 'apache/alias' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted mb-3">
-								use <code>${APACHE_LOCATION}</code> to Apache config inside current install path. <br/>
+								use <code>${APACHE_PATH}</code> to Apache config inside current install path. <br/>
 								use <code>${LARAGON_PATH}</code> to locate config inside current install path.
 							</small>
 						</div>
@@ -191,13 +202,13 @@ require __DIR__ . '/config.php';
 							<div class="input-group col-xs-12 col-md-6 mb-2">
 								<div class="input-group-prepend"><span class="input-group-text">HTTP</span></div>
 								<input type="number" class="form-control" name="apache[ports][http]" placeholder="80"
-									   required>
+									   required value="<?php echo get_option( 'apache/ports/http' ); ?>">
 							</div>
 
 							<div class="input-group col-xs-12 col-md-6 mb-2">
 								<div class="input-group-prepend"><span class="input-group-text">HTTPS</span></div>
 								<input type="number" class="form-control" name="apache[ports][https]" placeholder="443"
-									   required>
+									   required value="<?php echo get_option( 'apache/ports/https' ); ?>">
 							</div>
 						</div>
 					</div>
@@ -214,7 +225,7 @@ require __DIR__ . '/config.php';
 					</h2>
 				</div>
 
-				<div id="nginx_config_data" class="collapse" aria-labelledby="nginx_config"
+				<div id="nginx_config_data" class="collapse show" aria-labelledby="nginx_config"
 					 data-parent="#settings_accordion">
 					<div class="card-body">
 						<div class="form-row">
@@ -222,10 +233,12 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend">
 									<span class="input-group-text">Config Location</span>
 								</div>
-								<input type="text" class="form-control" name="nginx[config]" placeholder="${LARAGON_PATH}/etc/nginx/"
-									   required>
+								<input type="text" class="form-control" name="nginx[config]"
+									   placeholder="${LARAGON_PATH}/etc/nginx/"
+									   required value="<?php echo get_option( 'nginx/config' ); ?>">
 							</div>
-							<small class="col-xs-12 form-text text-muted mb-3"> use <code>${LARAGON_PATH}</code> to locate config
+							<small class="col-xs-12 form-text text-muted mb-3"> use <code>${LARAGON_PATH}</code> to
+								locate config
 								inside current install path</small>
 						</div>
 
@@ -235,10 +248,11 @@ require __DIR__ . '/config.php';
 									<span class="input-group-text">Sites Config</span>
 								</div>
 								<input type="text" class="form-control" name="nginx[sites_enabled]"
-									   placeholder="${NGINX_LOCATION}/sites-enabled/" required>
+									   placeholder="${NGINX_PATH}/sites-enabled/" required
+									   value="<?php echo get_option( 'nginx/sites_enabled' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted mb-3">
-								use <code>${NGINX_LOCATION}</code> to NGINX config inside current install path. <br/>
+								use <code>${NGINX_PATH}</code> to NGINX config inside current install path. <br/>
 								use <code>${LARAGON_PATH}</code> to locate config inside current install path.
 							</small>
 						</div>
@@ -248,11 +262,12 @@ require __DIR__ . '/config.php';
 								<div class="input-group-prepend">
 									<span class="input-group-text">Alias Config</span>
 								</div>
-								<input type="text" class="form-control" name="nginx[alias]" placeholder="${NGINX_LOCATION}/alias/"
-									   required>
+								<input type="text" class="form-control" name="nginx[alias]"
+									   placeholder="${NGINX_PATH}/alias/"
+									   required value="<?php echo get_option( 'nginx/alias' ); ?>">
 							</div>
 							<small class="col-xs-12 form-text text-muted mb-3">
-								use <code>${NGINX_LOCATION}</code> to Nginx config inside current install path. <br/>
+								use <code>${NGINX_PATH}</code> to Nginx config inside current install path. <br/>
 								use <code>${LARAGON_PATH}</code> to locate config inside current install path.
 							</small>
 						</div>
@@ -262,12 +277,14 @@ require __DIR__ . '/config.php';
 						<div class="form-row">
 							<div class="input-group col-xs-12 col-md-6 mb-2">
 								<div class="input-group-prepend"><span class="input-group-text">HTTP</span></div>
-								<input type="number" class="form-control" name="nginx[ports][http]" placeholder="80" required>
+								<input type="number" class="form-control" name="nginx[ports][http]" placeholder="80"
+									   required value="<?php echo get_option( 'nginx/ports/http' ); ?>">
 							</div>
 
 							<div class="input-group col-xs-12 col-md-6 mb-2">
 								<div class="input-group-prepend"><span class="input-group-text">HTTPS</span></div>
-								<input type="number" class="form-control" name="nginx[ports][https]" placeholder="443" required>
+								<input type="number" class="form-control" name="nginx[ports][https]" placeholder="443"
+									   required value="<?php echo get_option( 'nginx/ports/https' ); ?>">
 							</div>
 						</div>
 					</div>
@@ -276,7 +293,7 @@ require __DIR__ . '/config.php';
 
 		</div>
 
-
+		<button type="submit" class="btn btn-primary mt-3" name="update_settings">Update Settings</button>
 	</form>
 <?php
 
